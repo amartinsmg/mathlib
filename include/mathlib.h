50da9bf2d2e88eefa51d77e7ee208ea5b96021d6
amartinsmg <andremindurimg@gmail.com>
2026-03-17 19:57:59 -0300

    feat(mathlib): add aggregate header
    - Provide a single header for math lib
    - Includes all common math components
    - Simplifies integration and usage

---
+++ include/mathlib.h
@@ -0,0 +1,21 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+#include "armstrong_numbers.h"
+#include "basic_operations.h"
+#include "combinatorics.h"
+#include "factorial.h"
+#include "gcd.h"
+#include "geometry.h"
+#include "growth.h"
+#include "happy_numbers.h"
+#include "lcm.h"
+#include "percentage.h"
+#include "perfect_numbers.h"
+#include "prime_factorization.h"
+#include "prime_numbers.h"
+#include "probability_distribution.h"
+#include "statistics.h"
+#include "trigonometry.h"
+
+#endif /* MATHLIB_H */
