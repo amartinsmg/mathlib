5d6d2c9917d6303d98997908d9b2ac0051fb6b30
amartinsmg <andremindurimg@gmail.com>
2026-04-13 21:57:49 -0300

    refactor(math): consistent naming and include cleanup
    - Rename math_gaussianCDF to math_gaussian_CDF
    - Rename successProb to success_prob
    - Rename stdDev to std_dev

--- include/math/probability_distribution.h
+++ include/math/probability_distribution.h
@@ -5,27 +5,26 @@
 #ifndef PROBABILITY_DISTRIBUTION_H
 #define PROBABILITY_DISTRIBUTION_H
 
-#include <math.h>
 #include "combinatorics.h"
 
 /**
  * @brief Calculate the binomial probability mass function (PMF).
  *
  * @param trials The number of trials.
- * @param successProb The success probability.
+ * @param success_prob The success probability.
  * @param success The number of successful outcomes.
  *
  * @return The probability of exactly `success` successes in `trials` Bernoulli trials.
  */
 
-static inline double math_binominal(int trials, double successProb, int success)
+static inline double math_binominal(int trials, double success_prob, int success)
 {
   if (trials < 0 || success < 0 || success > trials ||
-      successProb < 0 || successProb > 1)
+      success_prob < 0 || success_prob > 1)
     return NAN;
   double result = math_combinationlf(trials, success) *
-                  pow(successProb, success) *
-                  pow((1 - successProb), (trials - success));
+                  pow(success_prob, success) *
+                  pow((1 - success_prob), (trials - success));
   return result;
 }
 
@@ -50,23 +49,23 @@ static inline double math_poisson(double lambda, int x)
  * @brief Calculate the cumulative distribution function (CDF) of a Gaussian distribution.
  *
  * @param mu The mean value of the Gaussian distribution.
- * @param stdDev The standard deviation of the Gaussian distribution.
+ * @param std_dev The standard deviation of the Gaussian distribution.
  * @param x The input value.
  *
  * @return The cumulative distribution function (CDF) value.
  */
 
-static inline double math_gaussianCDF(double mu, double stdDev, double x)
+static inline double math_gaussian_CDF(double mu, double std_dev, double x)
 {
-  if (stdDev < 0)
+  if (std_dev < 0)
     return NAN;
-  if (stdDev == 0)
+  if (std_dev == 0)
     return (x < mu) ? 0.0 : 1.0;
   double phi, result, z,
       denominator = 1,
       sum = 0;
   int i;
-  z = (x - mu) / stdDev;
+  z = (x - mu) / std_dev;
   phi = exp(-pow(z, 2) / 2) / sqrt(2 * M_PI);
   for (i = 1; i <= 100; i += 2)
   {
