96ef3102209d37f1d95d564919ac9dead622c4dc
amartinsmg <andremindurimg@gmail.com>
2026-04-13 21:56:49 -0300

    style(math): apply snake_case naming conventions
    - Rename functions to snake_case
    - Rename local variables to snake_case
    - Update math_mode signature and docs

--- include/math/statistics.h
+++ include/math/statistics.h
@@ -1,12 +1,11 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
-#include <stddef.h>
-#include <math.h>
 #include "basic_operations.h"
 #include "percentage.h"
 #include "sort.h"
 #include "vector.h"
+#include <stddef.h>
 
 /**
  * @brief Calculate the arithmetic mean of an array of values.
@@ -40,16 +39,16 @@ static inline double math_mean(const double *arr, size_t length)
  * @return The trimmed mean of the arr.
  */
 
-static inline double math_trimmedMean(const double *arr, size_t length, double percentage)
+static inline double math_trimmed_mean(const double *arr, size_t length, double percentage)
 {
   if (length == 0 || arr == NULL)
     return NAN;
-  size_t nElementsToTrim = (size_t)round(math_nPercentOfX(length, percentage));
-  if (2 * nElementsToTrim >= length)
+  size_t n_el_to_trim = (size_t)round(math_n_percent_of_x(length, percentage));
+  if (2 * n_el_to_trim >= length)
     return NAN;
   double result, *sortedArr = sort(arr, length);
-  arr += nElementsToTrim;
-  length -= 2 * nElementsToTrim;
+  arr += n_el_to_trim;
+  length -= 2 * n_el_to_trim;
   result = math_mean(arr, length);
   free(sortedArr);
   return result;
@@ -64,7 +63,7 @@ static inline double math_trimmedMean(const double *arr, size_t length, double p
  * @return The calculated geometric mean.
  */
 
-static inline double math_geometricMean(const double *arr, size_t length)
+static inline double math_geometric_mean(const double *arr, size_t length)
 {
   if (length == 0 || arr == NULL)
     return NAN;
@@ -77,7 +76,7 @@ static inline double math_geometricMean(const double *arr, size_t length)
       return NAN;
     product *= arr[i];
   }
-  result = math_nthRoot(product, length);
+  result = math_nth_root(product, length);
   return result;
 }
 
@@ -90,7 +89,7 @@ static inline double math_geometricMean(const double *arr, size_t length)
  * @return The calculated harmonic mean.
  */
 
-static inline double math_harmonicMean(const double *arr, size_t length)
+static inline double math_harmonic_mean(const double *arr, size_t length)
 {
   if (length == 0 || arr == NULL)
     return NAN;
@@ -121,12 +120,12 @@ static inline double math_median(const double *arr, size_t length)
   if (length == 0 || arr == NULL)
     return NAN;
   double result,
-      *sortedArr = sort(arr, length);
+      *sorted_arr = sort(arr, length);
   if (length % 2)
-    result = sortedArr[length / 2];
+    result = sorted_arr[length / 2];
   else
-    result = (sortedArr[length / 2 - 1] + sortedArr[length / 2]) / 2;
-  free(sortedArr);
+    result = (sorted_arr[length / 2 - 1] + sorted_arr[length / 2]) / 2;
+  free(sorted_arr);
   return result;
 }
 
@@ -145,11 +144,8 @@ typedef struct
  *
  * @param arr The array of values.
  * @param length The length of the array.
- * @param ptrNOfModes Pointer to store the number of modes found.
  *
- * @return An array containing the mode(s) of the input values.
- *
- * @note The number of modes is stored in ptrNOfModes.
+ * @return An Vector struct containing the mode(s) of the input values.
  */
 
 static inline Vector math_mode(const double *arr, size_t length)
@@ -159,37 +155,36 @@ static inline Vector math_mode(const double *arr, size_t length)
   if (length == 0 || arr == NULL)
     return modes;
 
-  double *sortedArr = sort(arr, length);
+  double *sorted_arr = sort(arr, length);
   Freq *frequencies = (Freq *)malloc(sizeof(*frequencies) * length);
   size_t i,
-      minFreq = __UINT32_MAX__,
-      maxFreq = 0,
-      resultLenght = 0,
+      min_freq = __UINT32_MAX__,
+      max_freq = 0,
       freq_len = 0;
 
-  frequencies[0].value = sortedArr[0];
+  frequencies[0].value = sorted_arr[0];
   frequencies[0].frequency = 1;
   freq_len++;
 
   for (i = 1; i < length; i++)
-    if (sortedArr[i] == frequencies[freq_len - 1].value)
+    if (sorted_arr[i] == frequencies[freq_len - 1].value)
       frequencies[freq_len - 1].frequency++;
     else
     {
-      frequencies[freq_len].value = sortedArr[i];
+      frequencies[freq_len].value = sorted_arr[i];
       frequencies[freq_len++].frequency = 1;
     }
-  free(sortedArr);
+  free(sorted_arr);
 
   for (i = 0; i < freq_len; i++)
   {
-    minFreq = frequencies[i].frequency < minFreq ? frequencies[i].frequency : minFreq;
-    maxFreq = frequencies[i].frequency > maxFreq ? frequencies[i].frequency : maxFreq;
+    min_freq = frequencies[i].frequency < min_freq ? frequencies[i].frequency : min_freq;
+    max_freq = frequencies[i].frequency > max_freq ? frequencies[i].frequency : max_freq;
   }
 
-  if (maxFreq > minFreq)
+  if (max_freq > min_freq)
     for (i = 0; i < freq_len; i++)
-      if (frequencies[i].frequency == maxFreq)
+      if (frequencies[i].frequency == max_freq)
         vector_append(&modes, &frequencies[i].value);
 
   free(frequencies);
@@ -303,7 +298,7 @@ static inline double math_variance(const double *arr, size_t length)
  * @return The calculated standard deviation.
  */
 
-static inline double math_stdDev(const double *arr, size_t length)
+static inline double math_std_dev(const double *arr, size_t length)
 {
   if (length == 0 || arr == NULL)
     return NAN;
@@ -320,7 +315,7 @@ static inline double math_stdDev(const double *arr, size_t length)
  * @return The calculated sample variance.
  */
 
-static inline double math_sampleVariance(const double *arr, size_t length)
+static inline double math_sample_variance(const double *arr, size_t length)
 {
   if (length <= 1)
     return NAN;
@@ -343,11 +338,11 @@ static inline double math_sampleVariance(const double *arr, size_t length)
  * @return The calculated sample standard deviation.
  */
 
-static inline double math_sampleStdDev(const double *arr, size_t length)
+static inline double math_sample_std_dev(const double *arr, size_t length)
 {
   if (length <= 1)
     return NAN;
-  double result = sqrt(math_sampleVariance(arr, length));
+  double result = sqrt(math_sample_variance(arr, length));
   return result;
 }
 
