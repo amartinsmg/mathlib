055a1dcdbee6bfdf59a1dc3ad07eee1b1987b452
amartinsmg <andremindurimg@gmail.com>
2026-04-13 22:04:35 -0300

    refactor(mathlib): Use snake_case for function names
    - Renamed functions to follow snake_case convention
    - Improved code readability and consistency

--- include/mathlib.hpp
+++ include/mathlib.hpp
@@ -13,122 +13,122 @@ namespace Math
 
 static inline double triangleArea1(double base, double height)
 {
-  return math_triangleArea1(base, height);
+  return math_triangle_area_1(base, height);
 }
 
 static inline double triangleArea2(double sideA, double sideB, double angleGama)
 {
-  return math_triangleArea2(sideA, sideB, angleGama);
+  return math_triangle_area_2(sideA, sideB, angleGama);
 }
 
 static inline double triangleArea3(double sideA, double sideB, double sideC)
 {
-  return math_triangleArea3(sideA, sideB, sideC);
+  return math_triangle_area_3(sideA, sideB, sideC);
 }
 
 static inline double squareArea(double side)
 {
-  return math_squareArea(side);
+  return math_square_area(side);
 }
 
 static inline double rectangleArea(double width, double length)
 {
-  return math_rectangleArea(width, length);
+  return math_rectangle_area(width, length);
 }
 
 static inline double rhombusArea(double largerDiagonal, double smallerDiagonal)
 {
-  return math_rhombusArea(largerDiagonal, smallerDiagonal);
+  return math_rhombus_area(largerDiagonal, smallerDiagonal);
 }
 
 static inline double parallelogramArea1(double base, double height)
 {
-  return math_parallelogramArea1(base, height);
+  return math_parallelogram_area_1(base, height);
 }
 
-static inline double parallelogramArea2(double sideA, double sideB, double angleBeetweenAB)
+static inline double parallelogramArea2(double sideA, double sideB, double angleBetweenAB)
 {
-  return math_parallelogramArea2(sideA, sideB, angleBeetweenAB);
+  return math_parallelogram_area_2(sideA, sideB, angleBetweenAB);
 }
 
 static inline double trapezoidArea(double largerBase, double smallerBase, double height)
 {
-  return math_trapezoidArea(largerBase, smallerBase, height);
+  return math_trapezoid_area(largerBase, smallerBase, height);
 }
 
 static inline double regPolygonArea(double side, int nOfSides)
 {
-  return math_regPolygonArea(side, nOfSides);
+  return math_reg_polygon_area(side, nOfSides);
 }
 
 static inline double circleArea(double radius)
 {
-  return math_circleArea(radius);
+  return math_circle_area(radius);
 }
 
 static inline double circularSectorArea1(double angle, double radius)
 {
-  return math_circularSectorArea1(angle, radius);
+  return math_circular_sector_area_1(angle, radius);
 }
 
 static inline double circularSectorArea2(double arc, double radius)
 {
-  return math_circularSectorArea2(arc, radius);
+  return math_circular_sector_area_2(arc, radius);
 }
 
 static inline double ellipseArea(double semiMajorAxis, double semiMinorAxis)
 {
-  return math_ellipseArea(semiMajorAxis, semiMinorAxis);
+  return math_ellipse_area(semiMajorAxis, semiMinorAxis);
 }
 
 static inline double cubeArea(double side)
 {
-  return math_cubeArea(side);
+  return math_cube_area(side);
 }
 
 static inline double cuboidArea(double width, double length, double height)
 {
-  return math_cuboidArea(width, length, height);
+  return math_cuboid_area(width, length, height);
 }
 
 static inline double prismArea(double baseArea, double basePerimeter, double height)
 {
-  return math_prismArea(baseArea, basePerimeter, height);
+  return math_prism_area(baseArea, basePerimeter, height);
 }
 
 static inline double regularPrismArea(double baseSide, int nOfBaseSides, double height)
 {
-  return math_regularPrismArea(baseSide, nOfBaseSides, height);
+  return math_reg_prism_area(baseSide, nOfBaseSides, height);
 }
 
 static inline double pyramidArea(double baseArea, double basePerimeter, double slantHeight)
 {
-  return math_pyramidArea(baseArea, basePerimeter, slantHeight);
+  return math_pyramid_area(baseArea, basePerimeter, slantHeight);
 }
 
 static inline double regPyramidArea(double baseSide, int nOfBaseSides, double height)
 {
-  return math_regPyramidArea(baseSide, nOfBaseSides, height);
+  return math_reg_pyramid_area(baseSide, nOfBaseSides, height);
 }
 
 static inline double cylinderArea(double baseRadius, double height)
 {
-  return math_cylinderArea(baseRadius, height);
+  return math_cylinder_area(baseRadius, height);
 }
 
 static inline double coneArea(double baseRadius, double height)
 {
-  return math_coneArea(baseRadius, height);
+  return math_cone_area(baseRadius, height);
 }
 
 static inline double sphereArea(double radius)
 {
-  return math_sphereArea(radius);
+  return math_sphere_area(radius);
 }
 
 static inline bool isArmstrong(long long num)
 {
-  return math_isArmstrong(num);
+  return math_is_armstrong(num);
 }
 
 static inline double logarithm(double num, double base)
@@ -138,12 +138,12 @@ static inline double logarithm(double num, double base)
 
 static inline double nthRoot(double radicand, double degree)
 {
-  return math_nthRoot(radicand, degree);
+  return math_nth_root(radicand, degree);
 }
 
 static inline double roundTo(double num, unsigned decimalPlaces)
 {
-  return math_roundTo(num, decimalPlaces);
+  return math_round_to(num, decimalPlaces);
 }
 
 static inline unsigned long long permutation(unsigned num)
@@ -153,7 +153,7 @@ static inline unsigned long long permutation(unsigned num)
 
 static inline unsigned long long cyclePermutation(unsigned num)
 {
-  return math_cyclePermutation(num);
+  return math_cycle_permutation(num);
 }
 
 static inline unsigned long long arrangement(unsigned total, unsigned selected)
@@ -173,7 +173,7 @@ static inline double permutationlf(unsigned num)
 
 static inline double cyclePermutationlf(unsigned num)
 {
-  return math_cyclePermutationlf(num);
+  return math_cycle_permutationlf(num);
 }
 
 static inline double arrangementlf(unsigned total, unsigned selected)
@@ -203,17 +203,17 @@ static inline long long gcd(long long x, long long y)
 
 static inline double deg2rad(double degrees)
 {
-  return math_deg2rad(degrees);
+  return math_deg_to_rad(degrees);
 }
 
 static inline double rad2deg(double radians)
 {
-  return math_rad2deg(radians);
+  return math_rad_to_deg(radians);
 }
 
 static inline double distancePoints(Point a, Point b)
 {
-  return math_distancePoints(a, b);
+  return math_distance_points(a, b);
 }
 
 static inline Point midpoint(Point a, Point b)
@@ -223,72 +223,72 @@ static inline Point midpoint(Point a, Point b)
 
 static inline double slopeLine(Point a, Point b)
 {
-  return math_slopeLine(a, b);
+  return math_slope_line(a, b);
 }
 
 static inline double inclinationLine(Point a, Point b)
 {
-  return math_inclinationLine(a, b);
+  return math_inclination_line(a, b);
 }
 
 static inline double lineYIntercept(Point a, Point b)
 {
-  return math_lineYIntercept(a, b);
+  return math_line_y_intercept(a, b);
 }
 
 static inline double distancePointLine(double inclination, double yIntercept, Point p)
 {
-  return math_distancePointLine(inclination, yIntercept, p);
+  return math_distance_point_line(inclination, yIntercept, p);
 }
 
 static inline double circlePerimeter(double radius)
 {
-  return math_circlePerimeter(radius);
+  return math_circle_perimeter(radius);
 }
 
 static inline int polygonDiagonals(int nOfSides)
 {
-  return math_polygonDiagonals(nOfSides);
+  return math_polygon_diagonals(nOfSides);
 }
 
 static inline double convexPolySumIntAng(double nOfSides)
 {
-  return math_convexPolySumIntAng(nOfSides);
+  return math_convex_poly_sum_i_ang(nOfSides);
 }
 
 static inline double regPolygonIAng(int nOfSides)
 {
-  return math_regPolygonIAng(nOfSides);
+  return math_reg_polygon_i_ang(nOfSides);
 }
 
 static inline double convexPolyExtAngle(int nOfSides)
 {
-  return math_convexPolyExtAngle(nOfSides);
+  return math_convex_poly_ext_ang(nOfSides);
 }
 
 static inline double simpGrowth(double initial, double rate, double interval)
 {
-  return math_simpGrowth(initial, rate, interval);
+  return math_simp_growth(initial, rate, interval);
 }
 
 static inline double simpGrowthRate(double initial, double final, double interval)
 {
-  return math_simpGrowthRate(initial, final, interval);
+  return math_simp_growthRate(initial, final, interval);
 }
 
 static inline double compGrowth(double initial, double rate, double interval)
 {
-  return math_compGrowth(initial, rate, interval);
+  return math_comp_growth(initial, rate, interval);
 }
 
 static inline double compGrowthRate(double initial, double final, double interval)
 {
-  return math_compGrowthRate(initial, final, interval);
+  return math_comp_growthRate(initial, final, interval);
 }
 
 static inline bool isHappy(long long num)
 {
-  return math_isHappy(num);
+  return math_is_happy(num);
 }
 
 static inline long long lcm(long long x, long long y)
@@ -298,22 +298,22 @@ static inline long long lcm(long long x, long long y)
 
 static inline double nPercentOfX(double x, double n)
 {
-  return math_nPercentOfX(x, n);
+  return math_n_percent_of_x(x, n);
 }
 
 static inline double nIsWhatPercentOfX(double x, double n)
 {
-  return math_nIsWhatPercentOfX(x, n);
+  return math_n_is_what_percent_of_x(x, n);
 }
 
 static inline bool isPerfect(long long num)
 {
-  return math_isPerfect(num);
+  return math_is_perfect(num);
 }
 
 static inline std::vector<long long> primeFactors(long long num)
 {
-  Vector out = math_primeFactors(num);
+  Vector out = math_prime_factors(num);
   long long *arr = (long long *)vector_get_values(&out);
   std::vector<long long> result(arr, arr + out.length);
   vector_free(&out);
@@ -323,7 +323,7 @@ static inline std::vector<long long> primeFactors(long long num)
 
 static inline bool isPrime(long long num)
 {
-  return math_isPrime(num);
+  return math_is_prime(num);
 }
 
 static inline double binominal(int trials, double successProb, int success)
@@ -338,7 +338,7 @@ static inline double poisson(double lambda, unsigned x)
 
 static inline double gaussianCDF(double mu, double stdDev, double x)
 {
-  return math_gaussianCDF(mu, stdDev, x);
+  return math_gaussian_CDF(mu, stdDev, x);
 }
 
 static inline double mean(const std::vector<double> v)
@@ -348,17 +348,17 @@ static inline double mean(const std::vector<double> v)
 
 static inline double trimmedMean(const std::vector<double> v, double percentage)
 {
-  return math_trimmedMean(v.data(), v.size(), percentage);
+  return math_trimmed_mean(v.data(), v.size(), percentage);
 }
 
 static inline double geometricMean(const std::vector<double> v)
 {
-  return math_geometricMean(v.data(), v.size());
+  return math_geometric_mean(v.data(), v.size());
 }
 
 static inline double harmonicMean(const std::vector<double> v)
 {
-  return math_harmonicMean(v.data(), v.size());
+  return math_harmonic_mean(v.data(), v.size());
 }
 
 static inline double median(const std::vector<double> v)
@@ -403,17 +403,17 @@ static inline double variance(const std::vector<double> v)
 
 static inline double stdDev(const std::vector<double> v)
 {
-  return math_stdDev(v.data(), v.size());
+  return math_std_dev(v.data(), v.size());
 }
 
 static inline double sampleVariance(const std::vector<double> v)
 {
-  return math_sampleVariance(v.data(), v.size());
+  return math_sample_variance(v.data(), v.size());
 }
 
 static inline double sampleStdDev(const std::vector<double> v)
 {
-  return math_sampleStdDev(v.data(), v.size());
+  return math_sample_std_dev(v.data(), v.size());
 }
 
 static inline double hypotenuse(double sideA, double sideB)
@@ -423,72 +423,72 @@ static inline double hypotenuse(double sideA, double sideB)
 
 static inline double sideRTriangle(double hypotenuse, double sideA)
 {
-  return math_sideRTriangle(hypotenuse, sideA);
+  return math_side_r_triangle(hypotenuse, sideA);
 }
 
 static inline double sideTriangleLC(double sideA, double sideB, double oppositeAng)
 {
-  return math_sideTriangleLC(sideA, sideB, oppositeAng);
+  return math_side_triangle_l_cos(sideA, sideB, oppositeAng);
 }
 
 static inline double angTriangleLC(double oppositeSide, double sideA, double sideB)
 {
-  return math_angTriangleLC(oppositeSide, sideA, sideB);
+  return math_ang_triangle_l_cos(oppositeSide, sideA, sideB);
 }
 
 static inline double sideTriangleLS(double oppositeAng, double sideA, double oppositeAng2A)
 {
-  return math_sideTriangleLS(oppositeAng, sideA, oppositeAng2A);
+  return math_side_triangle_l_sin(oppositeAng, sideA, oppositeAng2A);
 }
 
 static inline double angTriangleLS(double oppositeSide, double sideA, double oppositeAng2A)
 {
-  return math_angTriangleLS(oppositeSide, sideA, oppositeAng2A);
+  return math_ang_triangle_l_sin(oppositeSide, sideA, oppositeAng2A);
 }
 
 static inline double cubeVol(double side)
 {
-  return math_cubeVol(side);
+  return math_cube_vol(side);
 }
 
 static inline double cuboidVol(double length, double width, double height)
 {
-  return math_cuboidVol(length, width, height);
+  return math_cuboid_vol(length, width, height);
 }
 
 static inline double prismVol(double baseArea, double height)
 {
-  return math_prismVol(baseArea, height);
+  return math_prism_vol(baseArea, height);
 }
 
 static inline double regularPrismVol(double baseSide, int nOfBaseSides, double height)
 {
-  return math_regularPrismVol(baseSide, nOfBaseSides, height);
+  return math_reg_prism_vol(baseSide, nOfBaseSides, height);
 }
 
 static inline double pyramidVol(double baseArea, double height)
 {
-  return math_pyramidVol(baseArea, height);
+  return math_pyramid_vol(baseArea, height);
 }
 
 static inline double regPyramidVol(double baseSide, int nOfBaseSides, double height)
 {
-  return math_regPyramidVol(baseSide, nOfBaseSides, height);
+  return math_reg_pyramid_vol(baseSide, nOfBaseSides, height);
 }
 
 static inline double cylinderVol(double baseRadius, double height)
 {
-  return math_cylinderVol(baseRadius, height);
+  return math_cylinder_vol(baseRadius, height);
 }
 
 static inline double coneVol(double baseRadius, double height)
 {
-  return math_coneVol(baseRadius, height);
+  return math_cone_vol(baseRadius, height);
 }
 
 static inline double sphereVol(double radius)
 {
-  return math_sphereVol(radius);
+  return math_sphere_vol(radius);
 }
 
 } // namespace Math
