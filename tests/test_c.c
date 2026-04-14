4af49f0e9684d332000a8b7d5a0b8af82d0f55e8
amartinsmg <andremindurimg@gmail.com>
2026-04-13 22:05:35 -0300

    style(test): Reorder includes and rename variable

--- tests/test_c.c
+++ tests/test_c.c
@@ -1,6 +1,6 @@
+#include "mathlib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <mathlib.h>
 
 #define SIZE_OF_ARR 8
 
@@ -16,12 +16,12 @@ int main()
   double mode,
       arr[SIZE_OF_ARR] = {2, 4, 6, 7, 5, 3, 2, 1};
   Vector v = math_mode(arr, SIZE_OF_ARR);
-  size_t nOfModes = v.length;
+  size_t n_of_modes = v.length;
   mode = *(double *)v.data;
   vector_free(&v);
 
   ASSERT_TRUE(math_mean(arr, SIZE_OF_ARR) == 3.75);
-  ASSERT_TRUE(nOfModes == 1);
+  ASSERT_TRUE(n_of_modes == 1);
   ASSERT_TRUE(mode == 2);
   ASSERT_TRUE(math_gcd(12, 42) == 6);
 
