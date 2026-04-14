8696a5dc29523562443b8d3001851447ff685b38
amartinsmg <andremindurimg@gmail.com>
2026-04-13 21:57:15 -0300

    refactor(math): apply snake_case naming convention
    - Renamed all `math_...Vol` functions
    - Updated all function parameters to `snake_case`
    - Enhanced code readability and

--- include/math/volume.h
+++ include/math/volume.h
@@ -12,7 +12,7 @@
  * @return The volume of the cube.
  */
 
-static inline double math_cubeVol(double side)
+static inline double math_cube_vol(double side)
 {
   if (side < 0)
     return NAN;
@@ -30,7 +30,7 @@ static inline double math_cubeVol(double side)
  * @return The volume of the cuboid.
  */
 
-static inline double math_cuboidVol(double length, double width, double height)
+static inline double math_cuboid_vol(double length, double width, double height)
 {
   if (width < 0 || length < 0 || height < 0)
     return NAN;
@@ -41,102 +41,102 @@ static inline double math_cuboidVol(double length, double width, double height)
 /**
  * @brief Calculates the volume of a prism.
  *
- * @param baseArea The area of the prism's base.
+ * @param base_area The area of the prism's base.
  * @param height The height of the prism.
  *
  * @return The volume of the prism.
  */
 
-static inline double math_prismVol(double baseArea, double height)
+static inline double math_prism_vol(double base_area, double height)
 {
-  if (baseArea < 0 || height < 0)
+  if (base_area < 0 || height < 0)
     return NAN;
-  double result = baseArea * height;
+  double result = base_area * height;
   return result;
 }
 
 /**
  * @brief Calculates the volume of a regular prism.
  *
- * @param baseSide The length of the base side of the prism.
- * @param nOfBaseSides The number of sides of the base polygon of the prism.
+ * @param base_side The length of the base side of the prism.
+ * @param n_of_base_sides The number of sides of the base polygon of the prism.
  * @param height The height of the prism.
  *
  * @return The volume of the regular prism.
  */
 
-static inline double math_regularPrismVol(double baseSide, int nOfBaseSides, double height)
+static inline double math_reg_prism_vol(double base_side, int n_of_base_sides, double height)
 {
-  double baseArea = math_regPolygonArea(baseSide, nOfBaseSides),
-         result = math_prismVol(baseArea, height);
+  double base_area = math_reg_polygon_area(base_side, n_of_base_sides),
+         result = math_prism_vol(base_area, height);
   return result;
 }
 
 /**
  * @brief Calculates the volume of a pyramid.
  *
- * @param baseArea The area of the pyramid's base.
+ * @param base_area The area of the pyramid's base.
  * @param height The height of the pyramid.
  *
  * @return The volume of the pyramid.
  */
 
-static inline double math_pyramidVol(double baseArea, double height)
+static inline double math_pyramid_vol(double base_area, double height)
 {
-  if (baseArea < 0 || height < 0)
+  if (base_area < 0 || height < 0)
     return NAN;
-  double result = baseArea * height / 3;
+  double result = base_area * height / 3;
   return result;
 }
 
 /**
  * @brief Calculates the volume of a regular pyramid.
  *
- * @param baseSide The length of the base side of the pyramid.
- * @param nOfBaseSides The number of sides of the base polygon of the pyramid.
+ * @param base_side The length of the base side of the pyramid.
+ * @param n_of_base_sides The number of sides of the base polygon of the pyramid.
  * @param height The height of the pyramid.
  *
  * @return The volume of the regular pyramid.
  */
 
-static inline double math_regPyramidVol(double baseSide, int nOfBaseSides, double height)
+static inline double math_reg_pyramid_vol(double base_side, int n_of_base_sides, double height)
 {
-  double baseArea = math_regPolygonArea(baseSide, nOfBaseSides),
-         result = math_pyramidVol(baseArea, height);
+  double base_area = math_reg_polygon_area(base_side, n_of_base_sides),
+         result = math_pyramid_vol(base_area, height);
   return result;
 }
 
 /**
  * @brief Calculates the volume of a cylinder.
  *
- * @param baseRadius The radius of the cylinder's base.
+ * @param base_radius The radius of the cylinder's base.
  * @param height The height of the cylinder.
  *
  * @return The volume of the cylinder.
  */
 
-static inline double math_cylinderVol(double baseRadius, double height)
+static inline double math_cylinder_vol(double base_radius, double height)
 {
-  if (baseRadius < 0 || height < 0)
+  if (base_radius < 0 || height < 0)
     return NAN;
-  double result = math_circleArea(baseRadius) * height;
+  double result = math_circle_area(base_radius) * height;
   return result;
 }
 
 /**
  * @brief Calculates the volume of a cone.
  *
- * @param baseRadius The radius of the cone's base.
+ * @param base_radius The radius of the cone's base.
  * @param height The height of the cone.
  *
  * @return The volume of the cone.
  */
 
-static inline double math_coneVol(double baseRadius, double height)
+static inline double math_cone_vol(double base_radius, double height)
 {
-  if (baseRadius < 0 || height < 0)
+  if (base_radius < 0 || height < 0)
     return NAN;
-  double result = math_circleArea(baseRadius) * height / 3;
+  double result = math_circle_area(base_radius) * height / 3;
   return result;
 }
 
@@ -146,7 +146,7 @@ static inline double math_coneVol(double baseRadius, double height)
  * @return The volume of the sphere.
  */
 
-static inline double math_sphereVol(double radius)
+static inline double math_sphere_vol(double radius)
 {
   if (radius < 0)
     return NAN;
